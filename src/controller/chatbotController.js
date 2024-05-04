a0c5ddd13c0cdb6f2716b4a599ea4b77f613cb52
nhtan <hoangtan250997@gmail.com>
2024-05-04 18:20:02 +0700

    first commit

---
+++ src/controller/chatbotController.js
@@ -0,0 +1,17 @@
+let getHomePage=(req,res)=>{
+return res.send("Xin chao")
+};
+
+let getWebhook = (req, res)=>{
+
+}
+
+let postWebhook = (req, res)=>{
+
+}
+
+module.exports={
+    getHomePage: getHomePage,
+    getWebhook: getWebhook,
+    postWebhook: this.postWebhook // key:value
+}
